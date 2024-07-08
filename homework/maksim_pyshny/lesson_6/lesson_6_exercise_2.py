6ba47ce9e87ff4798d678a56b488c14c3b0ae363
Maksim Pyshny <maksimpyshny28@gmail.com>
2024-07-08 11:20:13 +0300

    homework for lesson 6 done

---
+++ homework/maksim_pyshny/lesson_6/lesson_6_exercise_2.py
@@ -0,0 +1,11 @@
+my_list = list(range(1, 101))
+
+for number in my_list:
+    if number % 3 == 0 and number % 5 == 0:
+        print('FuzzBuzz')
+    elif number % 3 == 0:
+        print('Fuzz')
+    elif number % 5 == 0:
+        print('Buzz')
+    else:
+        print(number)
